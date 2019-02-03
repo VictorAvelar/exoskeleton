7a012b78e4e74f39b13b1139574149c3dcc02252
victoravelar <deltatuts@gmail.com>
2019-02-03 16:10:09 +0100

    :tada: Initial commit

---
+++ tests/AbstractBaseTestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace VictorAvelar\Exoskeleton\Tests;
+
+use Phpunit\Framework\TestCase;
+
+abstract class AbstractBaseTestCase extends TestCase
+{
+    // your global methods here.
+}
